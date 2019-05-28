e907e8a15d1338f104a302231772c09af73e8572
unknown <veerabhadrudu.s@gmail.com>
2019-05-28 08:13:05 +0530

    Basic programs

---
+++ basic/hello-world-express.js
@@ -0,0 +1,13 @@
+var express_framework=require("express");
+var app=express_framework();
+var ip="0.0.0.0";
+var port=3000;
+
+
+app.get("/",(request,response) => {
+    response.end("Hello World using express Framework !");
+});
+
+app.listen(port,ip,()=>{
+    console.log(`Example app listening on port ${port}!`);
+});
