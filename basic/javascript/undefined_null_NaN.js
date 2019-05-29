0f6aff71efd2f8e0654ba39878b89fb75a56deec
unknown <veerabhadrudu.s@gmail.com>
2019-05-29 17:51:12 +0530

    Basic programs

--- basic/javascript/undefined_null_NaN.js
+++ basic/javascript/undefined_null_NaN.js
@@ -1,6 +1,6 @@
 // Null and undefined are equal.However ,there both types are not same. 
 // Undefined is not equal to any other value except null.
-// Null is used during manual clearing of value a variable.Undefined indicates error state.
+// Null is used during manual clearing of value of a variable.However,Undefined indicates error state.
 // NaN is value is generated/assigned during arithmetic operaion is failed.
 
 var var1,var2=null,num1=NaN;
