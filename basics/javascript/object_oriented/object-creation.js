10bfc6e907f15679e7a408d7c82edffbf09038cc
unknown <veerabhadrudu.s@gmail.com>
2019-06-01 17:46:30 +0530

    Basic programs

--- basics/javascript/object_oriented/object-creation.js
+++ basics/javascript/object_oriented/object-creation.js
@@ -1,5 +1,5 @@
 //Way's to create object.
-
+// Literal Notation.
 var person ={
     name:"Veera",
     age:"31",
@@ -7,14 +7,14 @@ var person ={
         console.log("Name of the person is "+this.name+" and age is "+this.age);
     }
 }
-
+//Using new Object 
 var person2=new Object()
 person2.name="Jhon"
 person2.age=25;
 person2.printDetails=function (){
     console.log("Name of the person is "+this.name+" and age is "+this.age);
 };
-
+//Using Object.create(prototype)
 var person3=Object.create(null);
 person3.name="Micheal"
 person3.age=52;
@@ -22,9 +22,21 @@ person3.printDetails=function (){
     console.log("Name of the person is "+this.name+" and age is "+this.age);
 };
 
+//Using constructor function.
+function Person(name,age){
+    this.name=name;
+    this.age=age;
+    this.printDetails=function (){
+        console.log("Name of the person is "+this.name+" and age is "+this.age);
+    };
+}
+
+//Out of all approaches , constructor function is the best approach.
+//it allows us to define template to create objects (by passing agruments to constructor function
+// during object creation) and it has it's own prototype object.
 person.printDetails();
 person2.printDetails();
 person3.printDetails();
-
+new Person("Abraham",21).printDetails();
 
 
