4217e0b8a058b7ebd2bf129d7071a43d6dcfd32a
unknown <veerabhadrudu.s@gmail.com>
2019-06-30 12:11:30 +0530

    Pitfalls of Module loading example in Node JS

--- basics/duplicate-module-loading.js
+++ basics/duplicate-module-loading.js
@@ -1,5 +1,8 @@
 //This example will workfine in Windows Operating system.
-// TO understand this example check - https://medium.com/@lazlojuly/are-node-js-modules-singletons-764ae97519af
+//In this example module is loaded twice even though we have only one module file
+//This behaviour like this in windows as it is a case insensitive operating system
+//However, this example will fail to work in linux machine due to case sensitive nature.
+//TO understand this example check - https://medium.com/@lazlojuly/are-node-js-modules-singletons-764ae97519af
 
 const counter1 = require("./counter.js");
 const counter2 = require("./Counter.js");
@@ -10,4 +13,4 @@ counter2.increment();
 
 console.log(counter1.value); // prints undefined as value is private
 console.log(counter1.get()); // prints 2
-console.log(counter2.get());
+console.log(counter2.get()); // pritns 1
