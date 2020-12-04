4fd17a284a78ed41ca9bed403adfae974f0e08b4
Veerabhadrudu <veerabhadrudu.s@hpe.com>
2020-12-04 23:15:57 +0530

    Updaing nodjs Mocha nested test case example

--- test/nested_test_cases.js
+++ test/nested_test_cases.js
@@ -1,5 +1,34 @@
 /* 
-    This is an example of nested testcases using mocha
+    This is an example of nested testcases using mocha.
+    
+    describe -> Is used to register the test case block such as before,beforeeach,it,after and aftereach.
+    before -> before block is always executed before execution of any it inside a describe block. 
+    beforeEach -> beforeEach block is executed before execution of each it inside a describe.
+    it -> it block is used for testing functionality. 
+    after -> after block is always executed after execution of all 'it' blocks inside a describe block.
+    afterEach -> afterEach block is executed after execution of each it block inside a describe block.
+
+    Mocha frame work execution flow:-
+    -> Describe block always registers blocks (it,after,before etc.) defined inside it into a queue.
+    -> Once all the registrations of blocks are completed i.e loading of test case file by mocha is completed 
+    then, Mocha framework starts executing those registered block from the queue.
+    -> If a descibe block is defined inside another descibe block during inital registration phase the blocks 
+    defined inside nested describe block are also get's registred into queue and later all those 
+    describe blocks are exeuted i.e after all registrtions are completed , mocha starts executing those 
+    registered blocks from the queue in the registered order from the queue.
+    ->In short first all describe blocks and nested describe blocks are executed (i.e registration phase of it,
+    after,before etc. defined inside describe blocks) and executes other statements ( i.e other javascript 
+    statements apart from test case blocks such as it,after,before etc.) written inside describe blocks are
+    executed along with registration process of test case blocks ( i.e it,after,before etc.). 
+    Finally Mocha framework starts executing registred test case blocks ( i.e it,after,before etc.) from the queue 
+    in the registred order.
+
+    -> For the other case , suppose if a describe is defined inside it block, That describe block is not 
+    initially executed by mocha framework.But later when 'it' block is executed from the queue (i.e after 
+    execution of all inital describe and nested descibe blocks are executed then stared executing 'it' blocks 
+    from the queue), mocha executes the that describe block (registers/pushes this 'it' blocks nested describle 
+    block test case blocks into queue end) and proceeds the execution.
+   
 */
 console.log("Log before definition of top describe");
 
@@ -7,12 +36,16 @@ describe("Top level describe", function () {
 
     console.log("Top level describe statement");
 
-    before("Top level before", function () {
-        console.log("Top level before statement");
-    });
-
     beforeEach("Top level before each", function () {
         console.log("Top level before each statement");
+    });
+
+    /* 
+    Remember before block is always executed before execution of any it inside a describe block.
+    However,beforeEach block is executed before execution of each it inside a describe.   
+    */
+    before("Top level before", function () {
+        console.log("Top level before statement");
     });
 
     it("Top level it", function () {
@@ -93,8 +126,8 @@ describe("Top level describe", function () {
             });
         });
 
-        it("Top level 2nd it", function () {
-            console.log("Top level 2nd it statement");
+        it("2nd it - inside first level describe", function () {
+            console.log("2nd it statement - inside first level describe");
         });
 
         describe("2nd Level describe", function () {
@@ -180,10 +213,10 @@ describe("Top level describe", function () {
 console.log("Log after definition of top describe");
 
 
-describe('Another top describe',function(){
+describe('Another top describe', function () {
     console.log('Another describe');
 
-    it('Another top describe it',function(){
+    it('Another top describe it', function () {
         console.log('Another top describe it statement')
     });
 
